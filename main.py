c95c98cd1e760c59c644b6314e4fa01cf993d8e7
Kevin <h.keving81@gmail.com>
2022-02-16 19:11:00 -0500

    - Add systems_radius and traffic_radius functions
    - Add traffic_report function

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import json
 import caching
 
-def traffic(system_name, file = './cache/traffic'):
+def traffic(system_name):
     # check if system traffic data is in cache already
     c = caching.Cache('traffic')
 
@@ -22,11 +22,56 @@ def traffic(system_name, file = './cache/traffic'):
         c.write(url, params, d)
         return d
 
+def systems_radius(center_system_name, radius):
+    # center_system_name - name of system at the center of radius search
+    url = "https://www.edsm.net/api-v1/sphere-systems"
+    params = {'systemName' : center_system_name, 'radius' : radius, 'showInformation' : 1}
+
+    r = requests.get(url, params = params)
+    d = json.loads(r.text)
+
+    return d
+
+def traffic_radius(center_system_name, radius, min_pop = -1):
+    # center_system_name - name of system which will be center of sphere
+    # radius - radius of the sphere
+    # get traffic data from systems in a sphere radius
+
+    systems = systems_radius(center_system_name, radius) 
+
+    r = []
+    for system in systems:
+        if 'population' in system['information']:
+            if system['information']['population'] > min_pop:
+                d = {'name' : system['name'],
+                        'traffic' : traffic(system['name'])['traffic']}
+                r.append(d)
+    
+    return r
+
+def traffic_report(dump_file_name, sys_name, radius, min_pop = -1):
+    # dump neat(er) traffic data into a text file
+    tr = traffic_radius(sys_name, radius, min_pop)
+    with open(dump_file_name, 'a') as f:
+        for report in tr:
+            f.write(json.dumps(report) + '\n')
+
+traffic_report('data.txt', 'Alcor', 20, min_pop = 20000)
 
 
-print(traffic('Warkawa'))
-print()
-print(traffic('Sol'))
-print()
-print(traffic('Itzamnets'))
+#for system in systems:
+#    sys_name = system['name']
+#
+#    try:
+#        sys_population = system['information']['population']
+#    
+#    except(KeyError):
+#        sys_population = 0
+#    
+#    if sys_population > 0:
+#        sys_traffic = str(traffic(sys_name)['traffic']['day'])
+#        print(sys_name + "   :   " + sys_traffic)
+
+
+
 
