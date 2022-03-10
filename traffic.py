cb8bbe31f83a1cbccd8afc20d1173f1f1473fa05
Kevin <h.keving81@gmail.com>
2022-03-10 14:52:28 -0500

    Model components needed for future changes

--- traffic.py
+++ traffic.py
@@ -7,6 +7,69 @@ import edsm
 # TODO: Update on a schedule
 # TODO: Cross-reference hour-by-hour traffic data to track players.
 
+"""
+ MODEL ShipRoute
+    Models ships suspected routes
+
+    attribute route:
+        returns dict containing route info
+
+    method add(system):
+        add system to route
+
+    method remove(system):
+        remove system from route
+
+ MODEL TrafficMonitor
+    Performs higher-level functions for the TrafficSpheres
+
+    attribute update_queue:
+        array of traffic objects in traffic queue
+
+    attribute routes:
+        returns array of possible routes
+
+    method create_sphere(name)
+        Creates TrafficSphere at system, adds to update queue
+        name - name of center system
+
+    method remove_sphere(name)
+        Removes sphere that has given system name as center from the update queue
+        name - name of center system
+
+    method run()
+        Add current center point to update_queue
+        Run updates until a traffic object detects a change in traffic data AND breakdown data (need ship name as an identifier)
+        Add entire sphere to update queue
+        Run updates until at least one traffic object detects a change 
+        Check if change in breakdown data corresponds to initlal change in breakdown data
+        If so, 
+            Log the ship name and its route using a Route model
+            Remove current sphere from update queue
+            Center current sphere at that system
+
+        
+
+    method track()
+
+"""
+
+"""
+MODEL TrafficSphere
+    Array of Traffic objects, set up as a sphere around a center system.
+
+    attribute center = traffic object at center of sphere
+
+    method get_radius(radius, name)
+        Gets Traffic objects within given radius of a system within sphere
+        name - name of system
+        radius - radius to search in (max distance)
+
+
+"""
+
+# TODO: add 'diff' method that detects changes in traffic data from last update
+
 class Traffic():
     def __init__(self, name):
         self.name = name
