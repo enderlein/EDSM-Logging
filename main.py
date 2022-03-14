6659f572de51ea36e511bcc48ea043c655c90082
Kevin <h.keving81@gmail.com>
2022-03-14 19:05:44 -0400

    Update example driver code

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import traffic
-import edsm
-import asyncio
+import time
+import json
 
 # gets traffic data for systems within sphere radius of given system, dumps to json file
 #tr = traffic.traffic_radius(system_name = 'Alcor', radius = 10, min_pop = 20000, cache = True)
 #print(tr)
 
-ts = traffic.TrafficSphere('Warkawa', 20)
+def _add_to(file, data):
+    with open(file, 'a') as f:
+        f.write(data + '\n')
 
-async def main():
-    print([t.traffic for t in ts.monitors.values()])
-    await ts.update()
-    print([t.traffic for t in ts.monitors.values()])
+def main():
+    ts = traffic.TrafficSphere('Warkawa', 20)
+    while True:
+        # update sphere
+        ts.update()
 
-    # input() is here to prevent bug in aiohttp module which raises 'RuntimeError: Event loop is closed' on exit
-    # Not sure, but I think this happens because aiohttp.ClientSession() (accessed in edsm.py) doesn't close
-    # itself fast enough (doesn't close before garbage collection happens)
-    input()
+        # dump to file
+        for monitor in ts.monitors.values():
+            _add_to('data.txt', json.dumps(monitor.traffic))
 
-asyncio.run(main())
+        time.sleep(20)
+
+        
