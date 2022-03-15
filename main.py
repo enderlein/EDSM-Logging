d8c170e04faa309b5c7642ac2e7dd3558005895f
Kevin <h.keving81@gmail.com>
2022-03-14 22:58:15 -0400

    Add some basic example driver code

--- main.py
+++ main.py
@@ -2,24 +2,46 @@ import traffic
 import time
 import json
 
-# gets traffic data for systems within sphere radius of given system, dumps to json file
-#tr = traffic.traffic_radius(system_name = 'Alcor', radius = 10, min_pop = 20000, cache = True)
-#print(tr)
-
-def _add_to(file, data):
+def append_file(file, data):
     with open(file, 'a') as f:
         f.write(data + '\n')
 
-def main():
-    ts = traffic.TrafficSphere('Warkawa', 20)
+def log_traffic(system_name):
+    # create TrafficMonitor object for given star system
+    tm = traffic.TrafficMonitor(system_name)
+
     while True:
-        # update sphere
-        ts.update()
+        # update monitor
+        tm.update()
 
-        # dump to file
-        for monitor in ts.monitors.values():
-            _add_to('data.txt', json.dumps(monitor.traffic))
+        # serialize traffic data (dict) into (str)
+        s = json.dumps(tm.traffic)
 
-        time.sleep(20)
+        # log in file
+        filename = f"traffic_{tm.traffic['system_name']}.txt"
+        append_file(filename, s)
 
-        
+        # sleep for 5 minutes
+        current_time = time.strftime("%H:%M:%S", time.localtime())
+        print(f"Sleeping: 5 minutes... (since {current_time})")
+        time.sleep(300)
+
+def detect_traffic_update(system_name):
+    # create TrafficMonitor object for given star system
+    tm = traffic.TrafficMonitor(system_name)
+
+    while True:
+        # update monitor
+        tm.update()
+
+        # check diff property
+        if tm.diff != None:
+            if tm.diff['traffic']['day'] > 0:
+                print(f"Update detected: {tm.diff['traffic']['day']} new ships\n" + 
+                              f"Location: {tm.diff['name']}\n" + 
+                              f"since last update (approx. {tm.diff['timestamp']} seconds ago)")
+                print(f"Ship breakdown: {tm.diff['breakdown']}")
+
+        #sleep for 30 seconds
+        print("Sleeping...")
+        time.sleep(30)
