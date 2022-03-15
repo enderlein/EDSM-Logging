b08092f8e594afc91a5c9c20c5b79bfcd99e4364
Kevin <h.keving81@gmail.com>
2022-03-15 19:56:23 -0400

    Begin writing tests

---
+++ tests/test_edsm.py
@@ -0,0 +1,86 @@
+import unittest
+
+from edsm import traffic
+from edsm import systems_radius
+
+class TrafficTest(unittest.TestCase):
+    def test_bad_name_Wawawa(self):
+        self.assertRaises(Exception, traffic, 'Wawawa')
+
+    def test_traffic_Sol(self):
+        json = traffic('Sol')
+
+        self.assertEqual(27, json['id'])
+        self.assertEqual(10477373803, json['id64'])
+        self.assertEqual('Sol', json['name'])
+        self.assertEqual('https://www.edsm.net/en/system/id/27/name/Sol', json['url'])
+
+        self.assertEqual('J. Calvert (Joshua)', json['discovery']['commander'])
+        self.assertEqual('2014-11-18 18:21:43', json['discovery']['date'])
+
+        self.assertIn('traffic', json)
+        self.assertIs(type(json['traffic']), dict)
+
+        self.assertIn('total', json['traffic'])
+        self.assertIs(type(json['traffic']['total']), int)
+
+        self.assertIn('week', json['traffic'])
+        self.assertIs(type(json['traffic']['week']), int)
+
+        self.assertIn('day', json['traffic'])
+        self.assertIs(type(json['traffic']['day']), int)
+
+        self.assertIn('breakdown', json)
+        self.assertIs(type(json['breakdown']), dict)
+        
+    def test_traffic_Warkawa(self):
+        json = traffic('Warkawa')
+
+        self.assertEqual(14026, json['id'])
+        self.assertEqual(18261798168017, json['id64'])
+        self.assertEqual('Warkawa', json['name'])
+        self.assertEqual('https://www.edsm.net/en/system/id/14026/name/Warkawa', json['url'])
+
+        self.assertEqual('sutex', json['discovery']['commander'])
+        self.assertEqual('2015-01-05 11:13:31', json['discovery']['date'])
+
+        self.assertIn('traffic', json)
+        self.assertIs(type(json['traffic']), dict)
+
+        self.assertIn('total', json['traffic'])
+        self.assertIs(type(json['traffic']['total']), int)
+
+        self.assertIn('week', json['traffic'])
+        self.assertIs(type(json['traffic']['week']), int)
+
+        self.assertIn('day', json['traffic'])
+        self.assertIs(type(json['traffic']['day']), int)
+
+        self.assertIn('breakdown', json)
+        self.assertIs(type(json['breakdown']), dict)
+
+    def test_traffic_warkawa_lowercase(self):
+        json = traffic('warkawa')
+
+        self.assertEqual(14026, json['id'])
+        self.assertEqual(18261798168017, json['id64'])
+        self.assertEqual('Warkawa', json['name'])
+        self.assertEqual('https://www.edsm.net/en/system/id/14026/name/Warkawa', json['url'])
+
+        self.assertEqual('sutex', json['discovery']['commander'])
+        self.assertEqual('2015-01-05 11:13:31', json['discovery']['date'])
+
+        self.assertIn('traffic', json)
+        self.assertIs(type(json['traffic']), dict)
+
+        self.assertIn('total', json['traffic'])
+        self.assertIs(type(json['traffic']['total']), int)
+
+        self.assertIn('week', json['traffic'])
+        self.assertIs(type(json['traffic']['week']), int)
+
+        self.assertIn('day', json['traffic'])
+        self.assertIs(type(json['traffic']['day']), int)
+
+        self.assertIn('breakdown', json)
+        self.assertIs(type(json['breakdown']), dict)
