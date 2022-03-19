239bffafd9e2d98a7c92414e4ee27f3147120a64
Kevin <h.keving81@gmail.com>
2022-03-19 18:20:41 -0400

    Rename examples.py to main.py

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import traffic
 import time
 import json
 
+# Turns out traffic API only updates once per commander per ship.
+# Can't track players in real time, but old traffic data might show popular routes??
+# Then again, its easy to opt out of being tracked, and anyone who would want to would, so
+# I'd really only be tracking players who wanted to be tracked
+# What to doooooooooooo
+
 def append_file(file, data):
     with open(file, 'a') as f:
         f.write(data + '\n')
 
-def log_traffic(system_name):
+def log_traffic(system_name, sleep):
     # create TrafficMonitor object for given star system
     tm = traffic.TrafficMonitor(system_name)
 
@@ -21,12 +27,12 @@ def log_traffic(system_name):
         filename = f"traffic_{tm.traffic['system_name']}.txt"
         append_file(filename, s)
 
-        # sleep for 5 minutes
+        # sleep
         current_time = time.strftime("%H:%M:%S", time.localtime())
-        print(f"Sleeping: 5 minutes... (since {current_time})")
-        time.sleep(300)
+        print(f"Sleeping: {sleep} seconds... (since {current_time})")
+        time.sleep(sleep)
 
-def detect_traffic_update(system_name):
+def detect_traffic_update(system_name, sleep):
     # create TrafficMonitor object for given star system
     tm = traffic.TrafficMonitor(system_name)
 
@@ -39,10 +45,10 @@ def detect_traffic_update(system_name):
             if tm.diff['traffic']['day'] > 0:
                 # instead of printing, you could return a value to a watchdog somewhere
                 print(f"Update detected: {tm.diff['traffic']['day']} new ships\n" + 
-                              f"Location: {tm.diff['name']}\n" + 
+                              f"Location: {tm.diff['system_name']}\n" + 
                               f"since last update (approx. {tm.diff['timestamp']} seconds ago)")
                 print(f"Ship breakdown: {tm.diff['breakdown']}")
 
         #sleep for 30 seconds
         print("Sleeping...")
-        time.sleep(30)
+        time.sleep(sleep)
