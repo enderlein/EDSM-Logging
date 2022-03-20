0cdc38a728dc42d225a3f82497261fe6a97886c1
Kevin <h.keving81@gmail.com>
2022-03-19 20:50:39 -0400

    Apply the models

---
+++ main.py
@@ -0,0 +1,44 @@
+import traffic
+import json
+import time 
+from concurrent.futures import ThreadPoolExecutor
+
+class SphereLogger():
+    def __init__(self, sphere: traffic.TrafficSphere, sleep):
+        self.sleep = sleep
+        self.traffic_sphere = sphere
+    
+    def append_file(self, file, data):
+        with open(file, 'a') as f:
+            f.write(data + '\n')
+
+    def run(self):
+        while True:
+            self.traffic_sphere.update_all()
+            filename = f"traffic_sphere_centered@{self.traffic_sphere.center}"
+            for traffic_monitor in self.traffic_sphere.monitors:
+                s = json.dumps(traffic_monitor.traffic)
+                
+                
+                self.append_file(filename, s)
+
+            current_time = time.strftime("%H:%M:%S", time.localtime())
+            print(f"{self} :Sleeping: for {self.sleep} seconds (since {current_time})")
+            time.sleep(self.sleep)
+
+
+def get_loggers(*names, radius, sleep):
+    spheres = [traffic.TrafficSphere(name, radius) for name in names]
+    loggers = [SphereLogger(sphere, sleep) for sphere in spheres]
+
+    return loggers
+
+def main():
+    loggers = get_loggers('Warkawa', 'Sol', radius = 8, sleep = 20)
+
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        for l in loggers:
+            executor.submit(l.run)
+
+if __name__ == "__main__":
+    main()
