ed8e11eb8b6c681406c86336ced6e093f79deffc
Kevin <h.keving81@gmail.com>
2022-03-20 20:54:41 -0400

    Add USER_AGENT var for use with requests

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 # maximum number of threads to be used by ThreadPoolExecutor objects
 # default = 20
 MAX_THREADS = 20
+
+# Will be defined as 'User-Agent' in the header for all API requests
+USER_AGENT = "TrafficHistory"
