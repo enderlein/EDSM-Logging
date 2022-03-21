c777ccf85e699a31e9ad5d3bd2c2588bdb27fd88
Kevin <h.keving81@gmail.com>
2022-03-21 19:17:57 -0400

    Implement System/factions endpoint (method System.factions)
    Also, add showAll option to Systems.sphere_systems

--- edsm.py
+++ edsm.py
@@ -68,21 +68,54 @@ class System():
 
         return query(self.url_base + endpoint, params)
 
+    @classmethod
+    def factions(self, systemName, showHistory = 0):
+        """
+        systemName* (string) - name of system
+        showHistory (int) - show factions history (0 : False, 1 : True)
+
+        returns (dict)
+
+        Queries EDSM to get information on stations in a given system
+        """
+
+        endpoint = "factions"
+        params = {'systemName' : systemName, 'showHistory' : showHistory}
+
+        return query(self.url_base + endpoint, params)
+
 class Systems():
     url_base = "https://www.edsm.net/api-v1/"
 
     @classmethod
     def sphere_systems(self, systemName, radius, showId = 0, 
         showCoordinates = 0, showPermit = 0, showInformation = 0, 
-        showPrimaryStar = 0, includeHidden = 0):
+        showPrimaryStar = 0, includeHidden = 0, showAll = 0):
         """
         systemName* (string) - name of system at the center of the radius
         radius* (int) - radius of search sphere (in lightyears)
+
+        showId (int) - (0 : False, 1 : True)
+        showCoordinates (int) - (0 : False, 1 : True)
+        showPermit (int) - (0 : False, 1 : True)
+        showInformation (int) - (0 : False, 1 : True)
+        showPrimaryStar (int) - (0 : False, 1 : True)
+        includeHidden (int) - (0 : False, 1 : True)
+
+        showAll (int) - 0 : False, 1 : True - whether to set all optional args to 1
 
         returns (dict)
 
         Queries EDSM to get information on systems within a sphere radius of given system
         """
+
+        if showAll:
+            showId = 1
+            showCoordinates = 1
+            showPermit = 1
+            showInformation = 1
+            showPrimaryStar = 1
+            includeHidden = 1
 
         endpoint = "sphere-systems"
         params = {'systemName' : systemName, 
