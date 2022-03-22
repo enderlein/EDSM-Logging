a08b2553b75020acf36d0aa861b45518511fae8f
Kevin <h.keving81@gmail.com>
2022-03-21 20:03:55 -0400

    Add models.py

---
+++ models.py
@@ -0,0 +1,10 @@
+"""
+I want these models to be 'clean' in the 'Pythonic' sense
+hide all the 'ugly' logic as best as possible
+
+MODEL class System:
+    __init__(self, system_name)
+
+    method 'create traffic logger':
+        
+"""
