b62d38602b48f7d85003eeb66ebe9d34551945b9
Kevin <h.keving81@gmail.com>
2022-03-31 21:24:03 -0400

    Implement SphereLogger class for logging data

--- main.py
+++ main.py
@@ -1,8 +1,64 @@
-import traffic
 import json
 import time 
+
 from concurrent.futures import ThreadPoolExecutor
 
+import models
+import edsm
+import config
+# TODO: Import config with from calls (not that big a module, but less overhead anyways)
+
+class SphereLogger():
+    def __init__(self, center, radius, delay):
+        self.filename = f'{center}_{radius}ly.data'
+        self.delay = delay
+        self.sphere = edsm.Systems.sphere_systems(center, radius, showAllInfo=1)
+        self._systems = None
+
+    @property
+    def systems(self):
+        if not self._systems:
+            self._systems = list(map(lambda d: models.System(d), self.sphere))
+
+        return self._systems
+
+    def update_traffic(self):
+        with ThreadPoolExecutor(max_workers = config.MAX_THREADS) as executor:
+            for system in self.systems:
+                executor.submit(system.traffic.update)
+
+    def update_stations(self):
+        with ThreadPoolExecutor(max_workers = config.MAX_THREADS) as executor:
+            for system in self.systems:
+                executor.submit(system.stations.update)
+        
+    def append_file(self, file, data):
+        with open(file, 'a') as f:
+            f.write(data + '\n')
+
+    def run(self):
+        while True:
+            self.update_traffic()
+            #self.update_stations()
+
+            timestamp = int(time.time())
+
+            for system in self.systems:
+                payload = {'name' : system.name,
+                            'id' : system.id,
+                            'id64' : system.id64,
+                            'coords' : system.coords,
+                            'traffic' : system.traffic.traffic['traffic'],
+                            'breakdown' : system.traffic.traffic['breakdown'],
+                            'timestamp' : timestamp}
+
+                s = json.dumps(payload)
+                self.append_file(self.filename, s)
+            
+            sleep_time = time.strftime("%H:%M:%S", time.localtime())
+            print(f"{self} :Sleeping: for {self.delay} seconds (since {sleep_time})")
+            time.sleep(self.delay)
+"""
 class SphereLogger():
     def __init__(self, sphere: traffic.TrafficSphere, sleep):
         self.sleep = sleep
@@ -42,3 +98,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+"""
