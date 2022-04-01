1852023988ae82e902e57c4b3bcc8a67d5f0fd15
Kevin <h.keving81@gmail.com>
2022-03-31 21:22:14 -0400

    Update comments

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,8 @@ import unittest
 
 from models import Traffic
 
+# TODO : Test update funcs (make sure objs are empty before update, + properly populated after update)
+
 class TrafficTest(unittest.TestCase):
     def test_Traffic_Sol(self):
         t = Traffic('Sol')
