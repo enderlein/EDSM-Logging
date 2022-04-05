638ec16644ce3cd582c221e1188a9b336553b1df
Kevin <h.keving81@gmail.com>
2022-04-05 15:45:23 -0400

    Initial intended implementation.
    SystemsLogger children will manage an array of <System> objs, takes optional args (in the form of a list of 'keys') on which pieces of data to capture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
-import time 
+import time
+import inspect
 
 from concurrent.futures import ThreadPoolExecutor
 
@@ -7,20 +8,45 @@ import models
 import edsm
 import config
 # TODO: Import config with from calls (not that big a module, but less overhead anyways)
+# TODO: Logs should be grouped. As is, data is saved as a bunch of loose json objects. 
+# They should be bundled by timestamp. to make comparison easier.
+class SystemsLogger():
+    def __init__(self, center, radius, delay, keys):
+        self.filename = f'{center}_{radius}ly.data' # TODO: there are better options for filename generation
+        self.keys = keys
 
-class SphereLogger():
-    def __init__(self, center, radius, delay):
-        self.filename = f'{center}_{radius}ly.data'
-        self.delay = delay
-        self.sphere = edsm.Systems.sphere_systems(center, radius, showAllInfo=1)
+        self._systems_data = None
         self._systems = None
+
+        # TODO: bad to have here, not general properties.
+        # center and radius should be defined in Child classes (where needed)
+        self.center = center
+        self.radius = radius
+        self.delay = delay
 
     @property
     def systems(self):
+        # Wrap objs from query data with models.System objects
+        # TODO: System objs should be persistent, not generated on the fly
         if not self._systems:
-            self._systems = list(map(lambda d: models.System(d), self.sphere))
+            self._systems = list(map(lambda d: models.System(d), self._systems_data))
 
         return self._systems
+
+    def gather_keys(self, system):
+        # creates dict of format {k : <System>.__dict__[k]} for k in keys
+        #d = dict(map(lambda k: (k, system.__dict__[k]), keys))
+        d = {}
+
+        for key in self.keys:
+            # TODO: add conditional for models.Stations objects
+            if isinstance(system.__dict__[key], models.Traffic):
+                d[key] = system.__dict__[key].dumpdict()
+
+            else:
+                d[key] = system.__dict__[key]
+   
+        return d
 
     def update_traffic(self):
         with ThreadPoolExecutor(max_workers = config.MAX_THREADS) as executor:
@@ -44,20 +70,22 @@ class SphereLogger():
             timestamp = int(time.time())
 
             for system in self.systems:
-                payload = {'name' : system.name,
-                            'id' : system.id,
-                            'id64' : system.id64,
-                            'coords' : system.coords,
-                            'traffic' : system.traffic.traffic,
-                            'breakdown' : system.traffic.breakdown,
-                            'timestamp' : timestamp}
+                payload = self.gather_keys(system)
 
+                payload['timestamp'] = timestamp
+                
                 s = json.dumps(payload)
                 self.append_file(self.filename, s)
             
             sleep_time = time.strftime("%H:%M:%S", time.localtime())
             print(f"{self} :Sleeping: for {self.delay} seconds (since {sleep_time})")
+
             time.sleep(self.delay)
+
+class SphereLogger(SystemsLogger):
+    def __init__(self, center, radius, delay, keys):
+        super().__init__(center, radius, delay, keys)
+        self._systems_data = edsm.Systems.sphere_systems(self.center, self.radius, showAllInfo=1)
 """
 class SphereLogger():
     def __init__(self, sphere: traffic.TrafficSphere, sleep):
