7c5170b9c1f2bdb6e80d266877d368b27a359bd4
Kevin <h.keving81@gmail.com>
2022-04-06 15:39:09 -0400

    Refactor for cleanliness + remove loop an d run()
    Cleanup
    
    but also, removed the while Loop under the run() method so user may implement their own timing/trigger methods.
    
    Renamed the run() method to log() (since it's no longer running)

--- main.py
+++ main.py
@@ -1,49 +1,53 @@
 import json
 import time
-import inspect
 
 from concurrent.futures import ThreadPoolExecutor
 
 import models
 import edsm
 import config
+
 # TODO: Import config with from calls (not that big a module, but less overhead anyways)
 # TODO: Logs should be grouped. As is, data is saved as a bunch of loose json objects. 
 # They should be bundled by timestamp. to make comparison easier.
+# TODO: logging
 
 # TODO: ABCs lol
 class SystemsLogger():
-    def __init__(self, delay, keys):
-        self.filename = f'{self}.data'
+    def __init__(self, keys):
         self.keys = keys
 
-        self.systems_data = None
         self._systems = None
 
-        self.delay = delay
+        # to be overwritten by children (TODO: ABCs lol)
+        self.filename = f'{self}.json'
+        self.systems_data = None
 
     @property
     def systems(self):
-        # Wrap objs from query data with models.System objects
         if not self._systems:
+            # Wrap system objs from query with models.System objects
             self._systems = list(map(lambda d: models.System(d), self.systems_data))
 
         return self._systems
 
+    def parse_key(self, obj, key):
+        if isinstance(obj.__dict__[key], models.Traffic):
+            return obj.__dict__[key].dumpdict()
+
+        return obj.__dict__[key]
+
     def gather_keys(self, system):
-        # creates dict of format {k : <System>.__dict__[k]} for k in keys
-        #d = dict(map(lambda k: (k, system.__dict__[k]), keys))
-        d = {}
+        # creates dict of format {k : self.parse_key(system, k) for k in self.keys}
+        return dict(map(lambda k: (k, self.parse_key(system, k)), self.keys))
 
-        for key in self.keys:
-            # TODO: add conditional for models.Stations objects
-            if isinstance(system.__dict__[key], models.Traffic):
-                d[key] = system.__dict__[key].dumpdict()
+    def update_by_keys(self):
+        # TODO: Find a better way to toggle these.
+        if 'traffic' in self.keys:
+            self.update_traffic()
 
-            else:
-                d[key] = system.__dict__[key]
-   
-        return d
+        if 'stations' in self.keys:
+            self.update_stations()
 
     def update_traffic(self):
         with ThreadPoolExecutor(max_workers = config.MAX_THREADS) as executor:
@@ -59,30 +63,29 @@ class SystemsLogger():
         with open(file, 'a') as f:
             f.write(data + '\n')
 
-    def run(self):
-        while True:
-            self.update_traffic()
-            #self.update_stations()
+    def log(self):
+        
+        self.update_by_keys()
 
-            timestamp = int(time.time())
+        timestamp = int(time.time())
 
-            for system in self.systems:
-                payload = self.gather_keys(system)
+        for system in self.systems:
+            # TODO: Rework output format, group by timestamp
+            # TODO: Generalize as generate_json() and write_json() methods
+            # so it's easier to implement other output formats (i.e csv)
+            payload = self.gather_keys(system)
 
-                payload['timestamp'] = timestamp
-                
-                s = json.dumps(payload)
-                self.append_file(self.filename, s)
+            payload['timestamp'] = timestamp
             
-            sleep_time = time.strftime("%H:%M:%S", time.localtime())
-            print(f"{self} :Sleeping: for {self.delay} seconds (since {sleep_time})")
-
-            time.sleep(self.delay)
+            s = json.dumps(payload)
+            self.append_file(self.filename, s)
+        
 
 class SphereLogger(SystemsLogger):
-    def __init__(self, center, radius, delay, keys):
-        super().__init__(delay, keys)
-        self.filename = f"{center} - {radius}ly.data"
+    def __init__(self, center, radius, keys):
+        super().__init__(keys)
+
+        self.filename = f"{center} - {radius}ly.json"
         self.systems_data = edsm.Systems.sphere_systems(center, radius, showAllInfo=1)
 """
 class SphereLogger():
