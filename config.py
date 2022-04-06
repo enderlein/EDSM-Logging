b5872eaf8cc51468030642305f9207e4526e769f
Kevin <h.keving81@gmail.com>
2022-04-06 18:37:38 -0400

    Add DEFAULT_SLEEP var

--- config.py
+++ config.py
@@ -2,5 +2,8 @@
 # default = 20
 MAX_THREADS = 20
 
-# Will be defined as 'User-Agent' in the header for all API requests
+# default time that logger objects will sleep for (when not specified)
+DEFAULT_SLEEP = 1800
+
+# Defines 'User-Agent' in the header for all API requests
 USER_AGENT = "TrafficHistory"
