ce725a95925449782e7b2d745f1f59d9cea708ca
Kevin <h.keving81@gmail.com>
2022-07-16 21:50:04 -0400

    Add comments

--- example.py
+++ example.py
@@ -1,13 +1,23 @@
 import edsm.log
 import edsm.api
 
-keys = {'system' : ['name', 'id', 'coords', 'information'], 'traffic' : ['traffic'], 'stations' : ['name', 'economy', 'market']}
+# Indicate which pieces of data to capture from which EDSM API endpoints by
+# building a dict with class names as keys and model attribute names as corresponding values
+# See docstrings under System(), Traffic(), and Station() classes in models.py for possible attribute names.
+keys = {'system' : ['name', 'id', 'coords', 'information'], 'traffic' : ['traffic', 'breakdown'], 'stations' : ['name', 'economy', 'market']}
 
+# create Logger object using keys
 logger = edsm.log.Logger(keys = keys)
+
+# Query edsm.net for star systems within 8 ly of star system 'Warkawa'
 sphere_data = edsm.api.Systems.sphere_systems('Warkawa', radius = 8, showAllInfo = True)
 
-for system in sphere_data:
-    logger.systems.add_system(system)
+# populate <edsm.log.Logger>.systems with systems data from sphere_data
+for system_data in sphere_data:
+    logger.systems.add_system(system_data)
 
+# set filename to 'test.json'
 logger.filename = 'test.json'
+
+# gets and appends requested data to file, creates file if it doesn't exist
 logger.log()
