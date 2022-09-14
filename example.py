b2e6433cf3135560a89bbff1744dd95f4e6901a7
eh <h.keving81@gmail.com>
2022-09-14 13:26:14 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,23 +1,40 @@
 import edsm.log
 import edsm.api
+import json
 
 # Indicate which pieces of data to capture from which EDSM API endpoints by
 # building a dict with class names as keys and model attribute names as corresponding values
 # See docstrings under System(), Traffic(), and Station() classes in models.py for possible attribute names.
-keys = {'system' : ['name', 'id', 'coords', 'information'], 'traffic' : ['traffic', 'breakdown'], 'stations' : ['name', 'economy', 'market']}
+keys = {'system' : ['name', 'coords'], 'traffic' : ['traffic', 'breakdown']}
+# keys = {'system' : ['name', 'id', 'coords', 'information'], 'traffic' : ['traffic', 'breakdown'], 'stations' : ['name', 'economy', 'market']}
 
 # create Logger object using keys
 logger = edsm.log.Logger(keys = keys)
 
-# Query edsm.net for star systems within 8 ly of star system 'Warkawa'
-sphere_data = edsm.api.Systems.sphere_systems('Warkawa', radius = 8, showAllInfo = True)
-
-# populate <edsm.log.Logger>.systems with systems data from sphere_data
-for system_data in sphere_data:
-    logger.systems.add_system(system_data)
-
-# set filename to 'test.json'
+# set filename to 'test.json' (optional)
 logger.filename = 'test.json'
 
-# gets and appends requested data to file, creates file if it doesn't exist
+# query edsm.net for star systems within 8 ly of star system 'Sol'
+sphere_data = edsm.api.Systems.sphere_systems('Sol', radius = 8, showCoordinates=True)
+
+# populate <edsm.log.Logger>.systems with API data
+logger.systems.populate(sphere_data)
+
+# get and append requested data to file, creates file if it doesn't exist
 logger.log()
+
+"""***"""
+
+# Doing some stuff to the data
+MIN_TRAFFIC = 5
+with open(logger.filename, 'r') as f:
+    datalog = json.loads(f.read())
+
+    for item in datalog:
+        print(item['timestamp'])
+
+        for entry in item['data']:
+            traffic_count = entry['traffic']['traffic']['day']
+
+            if traffic_count >= MIN_TRAFFIC:
+                print(f"\t{entry['system']['name']} : {traffic_count}")
